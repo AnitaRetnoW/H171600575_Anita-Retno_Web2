5cae737f0a85c0563740d6a1d676852c5b0f0a4b
AnitaRetnoW <sundartianisa@gmail.com>
2019-09-26 21:21:47 +0800

    tugas4

---
+++ routes/web.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::any('login', function() {
+    if (request()->getMethod() == 'POST') {
+        $rules = ['captcha' => 'required|captcha'];
+        $validator = validator()->make(request()->all(), $rules);
+        if ($validator->fails()) {
+            echo '<p style="color: #ff0000;">Incorrect!</p>';
+        } else {
+            echo '<p style="color: #00ff30;">Matched :)</p>';
+        }
+    }
+
+    $form = '<form method="post" action="captcha-test">';
+    $form .= '<input type="hidden" name="_token" value="' . csrf_token() . '">';
+    $form .= '<p>' . captcha_img() . '</p>';
+    $form .= '<p><input type="text" name="captcha"></p>';
+    $form .= '<p><button type="submit" name="check">Check</button></p>';
+    $form .= '</form>';
+    return $form;
+});
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/kategori_artikel', 'Kategori_ArtikelController@index')->name('kategori_artikel.index');
+Route::get('/kategori_berita', 'Kategori_BeritaController@index')->name('kategori_berita.index');
+Route::get('/kategori_galeri', 'Kategori_GaleriController@index')->name('kategori_galeri.index');
+Route::get('/kategori_pengumuman', 'Kategori_PengumumanController@index')->name('kategori_pengumuman.index');
+Route::get('/artikel', 'ArtikelController@index')->name('artikel.index');
+Route::get('/berita', 'BeritaController@index')->name('berita.index');
+Route::get('/galeri', 'GaleriController@index')->name('galeri.index');
+Route::get('/pengumuman', 'PengumumanController@index')->name('pengumuman.index');
+
+
+Route::get('login', 'HomeController@myCaptcha')->name('myCaptcha');
+Route::post('login', 'HomeController@myCaptchaPost')->name('myCaptcha.post');
+Route::get('refresh_captcha', 'HomeController@refreshCaptcha')->name('refresh_captcha');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
